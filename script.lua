d9bdd669a84834ef740b8a959c6069ab355ccf1d
oktayjr <31kensukenagai@gmail.com>
2026-08-01 11:48:37 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,15 +1,15 @@
 local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/main/source.lua", true))()
 
 local Window = Luna:CreateWindow({
-	Name = "FORCES SİKTİ .gg/berkforces",
+	Name = "Torrycxn&Clooter",
 	Subtitle = nil,
 	LogoID = nil,
 	LoadingEnabled = true,
-	LoadingTitle = "OE HUB",
-	LoadingSubtitle = "by saldı",
+	LoadingTitle = "Torrycxn&Clooter HUB",
+	LoadingSubtitle = "by Torrycxn",
 	ConfigSettings = {
 		RootFolder = nil,
-		ConfigFolder = "OE Hub"
+		ConfigFolder = "Torrycxn&Clooter Hub"
 	},
 	KeySystem = false,
 	KeySettings = {}
